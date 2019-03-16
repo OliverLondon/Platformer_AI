31f33a7eb48bb00ca5c591c47130814d6f39c9ff
OliverLondon <47685945+OliverLondon@users.noreply.github.com>
2019-03-15 23:29:56 -0700

    Updated, less shapes to draw per map.

--- GameMap.cpp
+++ GameMap.cpp
@@ -1,9 +1,11 @@
 using namespace std;
 #include "GameMap.h"
-#include <GL\glew.h>
-#include <GL\freeglut.h>
 #include "Tiles.h"
 #include <vector>
+#include <SFML/Graphics.hpp>
+#include <Windows.h>
+#include <SFML/System.hpp>
+#include <SFML/Window.hpp>
 /*
 The corresponding id numbers:
 0 = air
@@ -16,39 +18,56 @@ The corresponding id numbers:
 GameMap::GameMap()
 {
 	currentMapNumber = 0;
-
+	//-----------------------------------------------------------------
 	world_1.reserve(9);//small flat
 	world_1 =
 	{
 		0,0,0,
-		0,0,0,
+		0,0,9,
 		1,1,1
 	};
-
+	world_1_tiles.resize(2);
+	convex_shape(world_1_tiles[0], 2, 0, 2, 2);
+	flag(world_1_tiles[1], 1, 2, 2, 1);//flag
+	world_1_tiles[0].setFillColor(sf::Color::Green);
+	//-----------------------------------------------------------------
 	world_2.reserve(20 * 3);//long flat
 	world_2 =
 	{
 		0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
-		0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+		0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,9,
 		1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1
 	};
+	world_2_tiles.resize(2);
+	convex_shape(world_2_tiles[0], 2, 0, 19, 2);
+	flag(world_2_tiles[1], 1, 19, 19, 1);//flag
+	world_2_tiles[0].setFillColor(sf::Color::Green);
 
+	//-----------------------------------------------------------------
 	world_3.reserve(10 * 5);//walls
 	world_3 = 
 	{
 		0,0,0,0,0,0,0,0,0,0,
 		0,0,0,0,0,0,0,0,0,0,
 		0,0,1,0,0,0,1,0,0,0,
-		0,0,1,0,0,0,1,0,0,0,
+		0,0,1,0,0,0,1,0,0,9,
 		1,1,1,1,1,1,1,1,1,1
 	};
+	world_3_tiles.resize(4);
+	convex_shape(world_3_tiles[0], 4, 0, 9, 4);
+	convex_shape(world_3_tiles[1], 2, 2, 2, 3);
+	convex_shape(world_3_tiles[2], 2, 6, 6, 3);
+	flag(world_3_tiles[3], 3, 9, 9, 3);//flag
+	world_3_tiles[0].setFillColor(sf::Color::Green);
+	world_3_tiles[1].setFillColor(sf::Color::Green);
+	world_3_tiles[2].setFillColor(sf::Color::Green);
+	//-----------------------------------------------------------------
 }
 
 
 GameMap::~GameMap()
 {
 }
-
 
 void GameMap::SetMap(int m) {
 	currentMapNumber = m;
@@ -136,5 +155,48 @@ void GameMap::FillMap(std::vector<sf::ConvexShape> *boardImage) {
 		default:
 			break;
 		}
+	}
+}
+
+
+void GameMap::DrawMap(sf::RenderWindow &window) {
+	switch (currentMapNumber) {
+		case 1:
+			for (int i = 0; i < 2; i++) {
+				window.draw(world_1_tiles[i]);
+			}
+			break;
+		case 2:
+			for (int i = 0; i < 2; i++) {
+				window.draw(world_2_tiles[i]);
+			}
+			break;
+		case 3:
+			for (int i = 0; i < 4; i++) {
+				window.draw(world_3_tiles[i]);
+			}
+			break;
+		default:
+			for (int i = 0; i < 2; i++) {
+				window.draw(world_1_tiles[i]);
+			}
+			break;
+	}
+}
+
+std::vector<sf::ConvexShape> GameMap::GetTiles() {
+	switch (currentMapNumber) {
+	case 1:
+		return world_1_tiles;
+		break;
+	case 2:
+		return world_2_tiles;
+		break;
+	case 3:
+		return world_3_tiles;
+		break;
+	default:
+		return world_1_tiles;
+		break;
 	}
 }
