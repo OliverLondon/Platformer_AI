aee9fb1b6da2be7420e6898896baf4e990cf4dde
OliverLondon <47685945+OliverLondon@users.noreply.github.com>
2019-03-15 23:33:19 -0700

    Smaller icon, more getters, ability to move

--- Player.cpp
+++ Player.cpp
@@ -4,13 +4,37 @@
 
 Player::Player()
 {
-	icon = sf::RectangleShape(sf::Vector2f(30.f, 60.f));
-	icon.setFillColor(sf::Color::Cyan);
+	icon = sf::RectangleShape(sf::Vector2f(20.f, 40.f));
+	icon.setFillColor(sf::Color::Black);
 }
 
 
 Player::~Player()
 {
+}
+
+void Player::Move_left(void) {
+	ax = -0.4f;
+}	
+
+void Player::Move_right(void) {
+
+	ax = 0.4f;
+}
+
+void Player::Jump(void) {
+	if (on_ground) {
+		ay = -6.f;
+		on_ground = false;
+	}
+}
+
+void Player::Attack(void) {
+
+}
+
+void Player::Step(GameMap &m, Player &p) {
+	Move(m,p);
 }
 
 void Player::Draw(sf::RenderWindow &window) const {
@@ -19,5 +43,16 @@ void Player::Draw(sf::RenderWindow &window) const {
 
 void Player::SetPos(sf::Vector2f v) {
 	position = {v};
+	icon.setPosition(v);
+}
+
+void Player::MovePos(sf::Vector2f v) {
+	icon.move(v);
+	position += v;
+}
+
+void Player::MovePos(float x, float y) {
+	icon.move(x,y);
+	position += sf::Vector2f(x, y);
 }
 
