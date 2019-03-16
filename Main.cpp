6b56cf45d0943de6a6e818b2037e43ace182fb7f
OliverLondon <47685945+OliverLondon@users.noreply.github.com>
2019-03-15 23:31:05 -0700

    Added in the player, controls, win condition

--- Main.cpp
+++ Main.cpp
@@ -9,6 +9,7 @@ using namespace std;
 #include "Player.h"
 #include <vector>
 
+
 int main(int argc, char* argv[]) {
 	//set the map
 	GameMap map;
@@ -16,30 +17,51 @@ int main(int argc, char* argv[]) {
 	Player player;
 	player.SetPos(map.GetStart());
 
-	//Create the map's image storage
-	int x = map.GetWidth();
-	int y = map.GetHeight();
-	int z = x * y;
-	std::vector<sf::ConvexShape> boardImage(z);
-	//set and color each tile
-	map.FillMap(&boardImage);
-
 	//make and run the game window
-	sf::RenderWindow window(sf::VideoMode(512, 256), "AI Platformer", sf::Style::Close);
+	sf::RenderWindow window(sf::VideoMode(704, 384), "AI Platformer", sf::Style::Close);
+	window.setFramerateLimit(30);
+	//
+	//sf::Text t = sf::Text(sf::String("position is"+player.position);
+	//
 	while (window.isOpen())
 	{
 		sf::Event event;
 		while (window.pollEvent(event))
 		{
 			if (event.type == sf::Event::Closed) {window.close();}
+			if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left)) {
+				player.Move_left();
+				printf("%s\n", "Left key pressed!");
+			}
+			if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right)) {
+				player.Move_right();
+				printf("%s\n", "Right key pressed!");
+			}
+			if (sf::Keyboard::isKeyPressed(sf::Keyboard::Space)) {
+				printf("%s\n", "Space key pressed!");
+				if (player.on_ground) {
+					player.Jump();
+				}
+			}
 		}
+		float xx = player.position.x;
+		float yy = player.position.y;
+		///printf("Player position: %f %f\n",xx,yy);
+
 
 		// clear the window with a color
-		window.clear(sf::Color::Red);
+		window.clear(sf::Color::White);
 		
 		// draw everything here...		
-		for (int i = 0; i < z; i++) {
-			window.draw(boardImage[i]);
+		map.DrawMap(window);
+		player.Step(map,player);
+		player.Draw(window);
+		
+		if (player.has_won) {//quit game if reach the end
+			window.close();
+		}
+		if (player.hp <= 0) {//if the player has died
+			window.close();
 		}
 
 		// end the current frame
