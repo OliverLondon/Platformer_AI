812f7463c886748b0e166a6934d442d6b422ba40
OliverLondon <47685945+OliverLondon@users.noreply.github.com>
2019-03-15 23:33:54 -0700

    More attributes. Step function. Movement methods.

--- Player.h
+++ Player.h
@@ -1,18 +1,31 @@
-#pragma once
 #include "Physics.h"
+#include "GameMap.h"
 #include <SFML/Graphics.hpp>
+#pragma once
 class Player :
 	public Physics
 {
 public:
 	Player();
 	~Player();
-	sf::RectangleShape icon;
+
 	int hp = 50;
 	int dmg = 5;
+	bool has_won = false;
+	bool on_ground = false;
 
+	sf::RectangleShape icon;
 	sf::Vector2f position;
 	void SetPos(sf::Vector2f);
+	void MovePos(sf::Vector2f);
+	void MovePos(float, float);
+
+	void Move_left(void);
+	void Move_right(void);
+	void Jump(void);
+	void Attack(void);
+
+	void Step(GameMap &, Player &);
 
 	void Draw(sf::RenderWindow &window) const;
 
