48372b4a46f65494c19589c348e723b2c47ef1f8
OliverLondon <47685945+OliverLondon@users.noreply.github.com>
2019-03-15 23:32:40 -0700

    Added friction, collision method

--- Physics.h
+++ Physics.h
@@ -1,16 +1,22 @@
-
-
+using namespace std;
+#include <vector>
+#include <SFML/Graphics.hpp>
+#include "GameMap.h"
+class Player;
 #pragma once
 class Physics
 {
 public:
 	Physics();
 	~Physics();
-	float ax, ay, x, y, g;//movement vectors
-	std::vector<float> pos;
-private:
-	void Move(float x, float y, float ax, float ay);
 
+	float ax, ay, vx, vy, g, f;//movement vectors
+	std::vector<float> pos;
+	
+
+	void Move(GameMap &, Player &p);
+	
+	void Collision(sf::Rect<float> &, Player &p);
 
 };
 
